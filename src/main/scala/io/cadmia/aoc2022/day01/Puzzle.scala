f44d42512e7bd82e06a7492a5484375a0be39176
Michael Smith <mike.smith1@ovo.com>
2022-12-02 07:54:49 +0000

    AOC2022 Day 1 - Part 1& 2 - Simple mutable solution

--- src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
+++ src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
@@ -1,18 +1,23 @@
 package io.cadmia.aoc2022.day01
 
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
-
 @main def solvePuzzle(): Unit =
-  var highestCalories = 0
   var currentCalories = 0
+  val calories = ListBuffer.empty[Int]
 
   Source.fromResource("aoc/2022/day01/input").getLines.foreach(line => {
     if (line.nonEmpty)
       currentCalories += line.toInt
     else
-      if (currentCalories > highestCalories)
-        highestCalories = currentCalories
+      calories.append(currentCalories)
       currentCalories = 0
   })
-  println(highestCalories)
+
+  val sortedCalories = calories.sorted
+
+  println("Calorie Count:")
+  printf("Largest Elf Snack Stash: %s\n", sortedCalories.takeRight(1).sum)
+  printf("Top 3 Elf Snack Stash Calories: %s\n", sortedCalories.takeRight(3).sum)
+
 
