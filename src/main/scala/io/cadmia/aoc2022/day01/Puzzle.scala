8990cbed9a9393198d72109ee5b0d0afc6ab530b
Michael Smith <mike.smith1@ovo.com>
2022-12-02 11:36:10 +0000

    AOC2022 - Day 1 - Refactor to functional solution + hopefully infinitely sized input?

--- src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
+++ src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
@@ -2,22 +2,27 @@ package io.cadmia.aoc2022.day01
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-@main def solvePuzzle(): Unit =
-  var currentCalories = 0
-  val calories = ListBuffer.empty[Int]
+import scala.util.Try
 
-  Source.fromResource("aoc/2022/day01/input").getLines.foreach(line => {
-    if (line.nonEmpty)
-      currentCalories += line.toInt
-    else
-      calories.append(currentCalories)
-      currentCalories = 0
-  })
+def processCalorieLines = (tuple: (Int, Vector[Int]), line: String) => {
+  val (currentCaloriesSum, leaderboard) = tuple
+  val currentLineCalories = Try(line.toInt).getOrElse(0)
+  if line.nonEmpty then
+    (currentCaloriesSum + currentLineCalories, leaderboard)
+  else
+    val (lowestCalorieCount, lowestCalorieCountPos) = leaderboard.zipWithIndex.min
+    (0, if currentCaloriesSum > lowestCalorieCount then leaderboard.updated(lowestCalorieCountPos, currentCaloriesSum) else leaderboard)
+  end if
+}
 
-  val sortedCalories = calories.sorted
+@main def solvePuzzle(): Unit = {
+  val leaderboardSize = 3
+  val initLeaderboard = Vector.fill(leaderboardSize)(0)
+  val caloriesLeaderboard = Source.fromResource("aoc/2022/day01/input").getLines
+    .foldLeft(0, initLeaderboard)(processCalorieLines)._2.sorted
 
   println("Calorie Count:")
-  printf("Largest Elf Snack Stash: %s\n", sortedCalories.takeRight(1).sum)
-  printf("Top 3 Elf Snack Stash Calories: %s\n", sortedCalories.takeRight(3).sum)
-
+  printf("Largest Elf Snack Stash: %s\n", caloriesLeaderboard.last)
+  printf("Top %s Elf Snack Stash Calories: %s\n", leaderboardSize, caloriesLeaderboard.takeRight(leaderboardSize).sum)
+}
 
