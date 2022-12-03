adcb782f3aec8dd62ac15ef62cdeec90cb894304
Michael Smith <mike.smith1@ovo.com>
2022-12-03 22:14:54 +0000

    AOC2022 - Day 1 - WIP Refactor

--- src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
+++ src/main/scala/io/cadmia/aoc2022/day01/Puzzle.scala
@@ -1,27 +1,26 @@
 package io.cadmia.aoc2022.day01
 
 import scala.io.Source
-import scala.util.Try
 
-def processCalorieLines = (tuple: (Int, Vector[Int]), line: String) => {
-  val (currentCaloriesSum, leaderboard) = tuple
-  val currentLineCalories = Try(line.toInt).getOrElse(0)
-  if line.nonEmpty then
-    (currentCaloriesSum + currentLineCalories, leaderboard)
-  else
-    val (lowestCalorieCount, lowestCalorieCountPos) = leaderboard.zipWithIndex.min
-    (0, if currentCaloriesSum > lowestCalorieCount then leaderboard.updated(lowestCalorieCountPos, currentCaloriesSum) else leaderboard)
-  end if
+lazy val LeaderboardSize = 3
+case class SnackStash(caloriesSum: Int = 0, leaderboard: Vector[Int] = Vector.fill(LeaderboardSize)(0)) {
+  private def updatedLeaderboard = (caloriesSum +: leaderboard).sorted.takeRight(LeaderboardSize)
+
+  def addSnack(snackCalories: String): SnackStash = {
+    snackCalories.toIntOption match
+      case None => SnackStash(caloriesSum, updatedLeaderboard)
+      case Some(calories) => SnackStash(caloriesSum + calories, leaderboard)
+  }
+
+  def finalLeaderboard = updatedLeaderboard
 }
 
 @main def solvePuzzle(): Unit = {
-  val leaderboardSize = 3
-  val initLeaderboard = Vector.fill(leaderboardSize)(0)
-  val caloriesLeaderboard = Source.fromResource("aoc/2022/day01/input").getLines
-    .foldLeft(0, initLeaderboard)(processCalorieLines)._2.sorted
+  val leaderboard = Source.fromResource("aoc/2022/day01/input").getLines
+    .foldLeft(SnackStash())({ case (snackStash, snackCalories) => snackStash.addSnack(snackCalories)})
 
   println("Calorie Count:")
-  printf("Largest Elf Snack Stash: %s\n", caloriesLeaderboard.last)
-  printf("Top %s Elf Snack Stash Calories: %s\n", leaderboardSize, caloriesLeaderboard.takeRight(leaderboardSize).sum)
+  println(s"Largest Elf Snack Stash: ${leaderboard.finalLeaderboard.last}")
+  printf(s"Top ${LeaderboardSize} Elf Snack Stash calories: ${leaderboard.finalLeaderboard.sum}")
 }
 
