17da088bbe86f423db1b4950f98b1996d91437fd
Michael Smith <mike.smith1@ovo.com>
2022-12-04 10:50:49 +0000

    AOC2022 - Day 4 - Tidy up line order

--- src/main/scala/io/cadmia/aoc2022/day04/Puzzle.scala
+++ src/main/scala/io/cadmia/aoc2022/day04/Puzzle.scala
@@ -11,16 +11,14 @@ def stringToInt(string: String): Int = {
     case Some(number) => number
     case None => throw new Exception("Unable to convert input to number")
 }
+def calculatePartOneScore(assignmentRanges: AssignmentRanges): Score = if (assignmentRanges(0).containsSlice(assignmentRanges(1)) || assignmentRanges(1).containsSlice(assignmentRanges(0))) 1 else 0
 
-def processLine(line: String): Assignments = line.split(",").flatMap(range => range.split("-").map(stringToInt)).toVector
+def calculatePartTwoScore(assignmentRanges: AssignmentRanges): Score = if (assignmentRanges(0).intersect(assignmentRanges(1)).nonEmpty) 1 else 0
 
 def calculatePartScores(assignmentRanges: AssignmentRanges): (Int, Int) = (calculatePartOneScore(assignmentRanges), calculatePartTwoScore(assignmentRanges))
 
 def makeAssignmentRanges(assignments: Assignments): AssignmentRanges = (assignments(0) to assignments(1), assignments(2) to assignments(3))
-
-def calculatePartOneScore(assignmentRanges: AssignmentRanges): Score = if (assignmentRanges(0).containsSlice(assignmentRanges(1)) || assignmentRanges(1).containsSlice(assignmentRanges(0))) 1 else 0
-
-def calculatePartTwoScore(assignmentRanges: AssignmentRanges): Score = if (assignmentRanges(0).intersect(assignmentRanges(1)).nonEmpty) 1 else 0
+def processLine(line: String): Assignments = line.split(",").flatMap(range => range.split("-").map(stringToInt)).toVector
 
 lazy val getLineScores = processLine.andThen(makeAssignmentRanges).andThen(calculatePartScores)
 
